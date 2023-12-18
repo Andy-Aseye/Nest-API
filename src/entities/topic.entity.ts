3e42cc3a0f24b36cf6cdab1af0498a966ac900de
Andy-Aseye <andyaseye@gmail.com>
2023-12-18 19:19:39 +0000

    first commit

---
+++ src/entities/topic.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Topic {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @OneToMany((type) => Comment, (comment) => comment.topic)
+  comments: Comment[];
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+  async findOne(id: number) {
+    return await this.userRepo.findOne({ where: { id: id } });
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userRepo.create(createUserDto);
+    return await this.userRepo.save(user);
+  }
+
+  async update() {
+    
+  }
+}
